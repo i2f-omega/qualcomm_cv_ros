Fix quaternion calcs for aruco localization

src/Marker.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import math
 import numpy as np
 from geometry_msgs.msg import Pose, Quaternion
 from tf.transformations import quaternion_from_euler
@@ -24,7 +25,11 @@ class Marker():
         self.pose.position.x = x
         self.pose.position.y = y
         self.pose.position.z = z
-        self.pose.orientation = Quaternion(*quaternion_from_euler(rot_x, rot_y, rot_z))
+        rot_x += math.pi/2
+        rot_y += 0
+        rot_z += math.pi/2
+
+        self.pose.orientation = Quaternion(*quaternion_from_euler(rot_x, rot_y+math.pi/2, rot_z))
         tmat = translation_matrix((x, y, z))
         qmat = quaternion_matrix(quaternion_from_euler(rot_x, rot_y, rot_z))
         self.tf_mat = np.dot(tmat, qmat)

src/aruco_localization.py
@@ -22,7 +22,7 @@ import cv2
 # For aruco markers
 from Marker import Marker
 import yaml
-from geometry_msgs.msg import Quaternion, Transform, Pose, PoseStamped
+from geometry_msgs.msg import Quaternion, Transform, Pose, PoseStamped, Vector3
 from tf.transformations import quaternion_from_euler, euler_from_quaternion, inverse_matrix
 from tf.transformations import translation_matrix, translation_from_matrix
 from tf.transformations import quaternion_matrix, quaternion_from_matrix
@@ -32,6 +32,7 @@ class ArucoLocalization():
     def __init__(self):
 
         sub_img_topic = "/hires/image_raw"
+        aruco_image_topic = "/detected_markers"
         pub_pose_topic = "/qualcomm/pose"
 
         cam_calibration_path = "/home/kcoble/catkin_ws/src/qualcomm_cv_ros/include/default_hires_calibration.yaml"
@@ -43,7 +44,7 @@ class ArucoLocalization():
 
         # Initialize publisher
         self.pub = rospy.Publisher(pub_pose_topic, PoseStamped, queue_size=10)
-        self.im_pub = rospy.Publisher("image_repub", Image, queue_size=1)
+        self.im_pub = rospy.Publisher(aruco_image_topic, Image, queue_size=1)
 
         # Initialize aruco details
         self.cam_mtx, self.distort_mtx = self.loadCoefficients(cam_calibration_path)
@@ -52,7 +53,7 @@ class ArucoLocalization():
         self.parameters = cv2.aruco.DetectorParameters_create()
 
         # Initialize subscriber to image stream
-        rospy.Subscriber(sub_img_topic, Image, self.img_callback, queue_size = 1)
+        rospy.Subscriber(sub_img_topic, Image, self.img_callback)#, queue_size = 1)
         rospy.sleep(0.5) # Delay briefly to allow subscribers to find messages
 
 
@@ -61,26 +62,25 @@ class ArucoLocalization():
         try:
             image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="mono8")
             image = cv2.resize(image, (640,480)) # Temporary until camera is calibrated properly
-            self.im_pub.publish(self.bridge.cv2_to_imgmsg(image, "passthrough"))
 
             ids, corners = self.detectMarkersInMap(image)
-            self.updatePoseFromMarkers(ids, corners)
+            cam_pose, image = self.updatePoseFromMarkers(ids, corners, image)
+            self.im_pub.publish(self.bridge.cv2_to_imgmsg(image, "passthrough"))
 
+            if cam_pose:
+                temp_pub = PoseStamped()
+                # temp_pub.pose = self.marker_dict["0"].pose
+                temp_pub.pose = cam_pose
+                temp_pub.header.frame_id = "map"
+                temp_pub.header.stamp = rospy.Time.now()
 
-
-
-            temp_pub = PoseStamped()
-            temp_pub.pose = self.marker_dict["2"].pose
-            temp_pub.header.frame_id = "map"
-            temp_pub.header.stamp = rospy.Time.now()
-
-            self.pub.publish(temp_pub)
+                self.pub.publish(temp_pub)
 
         except CvBridgeError as e:
             print(e)
 
 
-    def updatePoseFromMarkers(self, ids, corners):
+    def updatePoseFromMarkers(self, ids, corners, image):
         """
         NOTE:
         For now I am ignoring 6 DOF pose and am
@@ -98,7 +98,10 @@ class ArucoLocalization():
                 tvec = tvec[0][0]
 
                 tmat = translation_matrix((tvec[0], tvec[1], tvec[2]))
-                qmat = quaternion_matrix(quaternion_from_euler(rvec[0], rvec[1], rvec[2]))
+                qmat = np.zeros((4,4))
+                qmat[:3, :3], _ = cv2.Rodrigues(rvec)
+                qmat[3,3] = 1.
+
                 tf_cam2mark = np.dot(tmat, qmat)
                 tf_mark2cam = inverse_matrix(tf_cam2mark)
 
@@ -106,22 +109,23 @@ class ArucoLocalization():
 
                 tf_map2cam = np.dot(marker.tf_mat, tf_cam2mark)
                 # trans = translation_from_matrix(tf_map2cam)
-                trans = translation_from_matrix(tf_map2cam)
-                rot = euler_from_quaternion(quaternion_from_matrix(tf_mark2cam))
+                rot = euler_from_quaternion(quaternion_from_matrix(tf_map2cam))
+
+                cam_pose = Pose()
+                cam_pose.position = Vector3(*translation_from_matrix(tf_map2cam))
+                cam_pose.orientation = Quaternion(*quaternion_from_matrix(tf_map2cam))
+
                 print(marker.id)
-                print("tv", tvec)
-                print("rv",rvec)
-                print("t", trans)
-                print("r", rot)
+                print("cam pose", cam_pose)
 
-                # trans = Transform()
-                # trans.translation.x = tvec[0]
-                # trans.translation.y = tvec[1]
-                # trans.translation.z = tvec[2]
-                # trans.rotation = Quaternion(*quaternion_from_euler(
-                #     rvec[0], rvec[1], rvec[2]))
+                # Draw axis on marker and publish image
+                image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+                image = cv2.aruco.drawAxis(image, self.cam_mtx, self.distort_mtx, rvec, tvec, 0.1)
 
-                # print(trans)
+            return cam_pose, image
+        else:
+            return None, image
+
 
     def detectMarkersInMap(self, image):
         """